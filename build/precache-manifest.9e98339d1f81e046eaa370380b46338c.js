105bab8be794e65a77d8c22b555dd9ccbf6ec18f
Andy Chiu <andy.chiu@refinery29.com>
2018-10-06 15:59:27 -0400

    build

---
+++ build/precache-manifest.9e98339d1f81e046eaa370380b46338c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e6ae2ce2ea80ee69b4e",
+    "url": "/static/js/main.6e6ae2ce.chunk.js"
+  },
+  {
+    "revision": "af9074e2a1b2b8808559",
+    "url": "/static/js/1.af9074e2.chunk.js"
+  },
+  {
+    "revision": "6e6ae2ce2ea80ee69b4e",
+    "url": "/static/css/main.63005cfb.chunk.css"
+  },
+  {
+    "revision": "af9074e2a1b2b8808559",
+    "url": "/static/css/1.b8f3b7e4.chunk.css"
+  },
+  {
+    "revision": "25c9bae2b535e513118d33647b41ca9c",
+    "url": "/index.html"
+  }
+];
